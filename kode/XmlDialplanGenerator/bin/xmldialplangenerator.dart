fc34ed05a15b1f9cc6f400f92cea12f8934bc44b
Thomas Pedersen <s103665@student.dtu.dk>
2014-04-07 17:55:33 +0200

    Initial try on using LibDialplan.

--- kode/XmlDialplanGenerator/bin/xmldialplangenerator.dart
+++ kode/XmlDialplanGenerator/bin/xmldialplangenerator.dart
@@ -1,25 +1,119 @@
+import 'dart:convert';
+import 'dart:async';
 import 'package:xml/xml.dart';
+import 'package:libdialplan/libdialplan.dart';
 
 void main() {
-  print("Hello, World!");
+  Dialplan handplan;
+  try {
+    Map dialplan = JSON.decode(dialplan1);
+    handplan = new Dialplan.fromJson(dialplan)
+      ..receptionId = 1;
 
-  XmlElement root = new XmlElement('Dialplan');
-  XmlElement extension = new XmlElement('Extensions');
-  extension.attributes['wday'] = 'monday';
-  root.children.add(extension);
-  print(root);
+    //print(JSON.encode(handplan.toJson()));
 
-  XmlElement test =
-      XML.parse(
-      ''' <stackpanel>
-          <checkbox text='Hello World!' ></checkbox>
-          <textblock text='Hello World!' fontSize='12'></textblock>
-              <border>
-                  <image>
-                      The quick brown fox jumped over the lazy dog.
-                  </image>
-              </border>
-      </stackpanel>
-      '''
-      );
+  } catch(e, s) {
+    print('error $e');
+    print('stack $s');
+  }
+
+  List<XmlElement> extensions = generateXml(handplan);
+
+  extensions.forEach(print);
 }
+
+List<XmlElement> generateXml(Dialplan dialplan) {
+  List<XmlElement> nodes = new List<XmlElement>();
+
+  //The conditions.
+  Map<String, List<Condition>> conditions = extractConditions(dialplan);
+
+  //The extension the caller hits.
+  XmlElement entry = makeEntryNode(dialplan);
+  nodes.add(entry);
+
+  List<XmlElement> conditionVariableSetters = new List<XmlElement>();
+  conditions.forEach((k, v) => conditionVariableSetters.add((ConditionVariableSetter(k,v))));
+
+  return nodes;
+}
+
+XmlElement makeEntryNode(Dialplan dialplan) {
+  XmlElement entry = new XmlElement('extension')
+    ..attributes['name'] = 'r_${dialplan.receptionId}';
+
+  XmlElement numberCondition = XmlCondition('destination_number', '^${dialplan.entryNumber}\$');
+  entry.children.add(numberCondition);
+
+  XmlElement setId = XmlAction('set', 'receptionid=${dialplan.receptionId}');
+  XmlElement main = XmlAction('transfer', mainDestinationName(dialplan.receptionId));
+
+  numberCondition.children.addAll([setId, main]);
+
+  return entry;
+}
+
+XmlElement XmlCondition(String field, String expression) =>
+    new XmlElement('condition')
+  ..attributes['field'] = field
+  ..attributes['expression'] = expression;
+
+XmlElement XmlAction(String application, String data) =>
+    new XmlElement('action')
+    ..attributes['application'] = application
+    ..attributes['data'] = data;
+
+String mainDestinationName(int receptionId) => 'r_${receptionId}_main';
+
+Map<String, List<Condition>> extractConditions(Dialplan dialplan) {
+  Map<String, List<Condition>> conditions = new Map<String, List<Condition>>();
+
+  for(Extension extension in dialplan.Extensions) {
+    if(extension.conditions.isNotEmpty) {
+      conditions[extension.name] = extension.conditions;
+    }
+  }
+
+  return conditions;
+}
+
+XmlElement ConditionVariableSetter(String extensionName, List<Condition> conditions) {
+  XmlElement node = new XmlElement('extension');
+
+  return node;
+}
+
+String dialplan1 = '''
+{
+    "extensions": [
+        {
+            "name": "open",
+            "conditions": [
+                {
+                    "condition": "time",
+                    "minute-of-day": "480-1020",
+                    "wday": "mon-fre",
+                }
+            ],
+            "actions": [
+                {
+                    "action": "receptionists",
+                    "sleeptime": 0,
+                    "music": "mohrec7",
+                    "welcomefile": "r_7_welcome.wav"
+                }
+            ]
+        },
+        {
+            "name": "catchall",
+            "conditions": [],
+            "actions": [
+                {
+                    "action": "voicemail",
+                    "email": "voicemail@responsum.dk"
+                }
+            ]
+        }
+    ]
+}
+''';
