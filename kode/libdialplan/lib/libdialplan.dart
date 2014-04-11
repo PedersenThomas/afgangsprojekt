98fbcd3876868f16a7a3d764276bd32ff0cedaca
Thomas Pedersen <s103665@student.dtu.dk>
2014-04-11 18:41:40 +0200

    Ivr rename

--- kode/libdialplan/lib/libdialplan.dart
+++ kode/libdialplan/lib/libdialplan.dart
@@ -5,7 +5,7 @@ part 'dialplan/condition.dart';
 part 'dialplan/dialplan.dart';
 part 'dialplan/extension.dart';
 
-part 'dialplan/actions/ivr.dart';
+part 'dialplan/actions/execute_ivr.dart';
 part 'dialplan/actions/forward.dart';
 part 'dialplan/actions/playaudio.dart';
 part 'dialplan/actions/receptionists.dart';
