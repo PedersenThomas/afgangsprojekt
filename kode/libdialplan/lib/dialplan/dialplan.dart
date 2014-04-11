f6d9ac36d88b1cf53e1a2f656bd6de7b3672017e
Thomas Pedersen <s103665@student.dtu.dk>
2014-04-11 18:42:07 +0200

    Dialplan to and from Json implemented.

--- kode/libdialplan/lib/dialplan/dialplan.dart
+++ kode/libdialplan/lib/dialplan/dialplan.dart
@@ -8,12 +8,26 @@ class Dialplan {
   Dialplan();
 
   factory Dialplan.fromJson(Map json) {
-    Dialplan plan = new Dialplan()
-      ..Extensions.addAll((json['extensions'] as List<Map>).map((Map e) => new Extension.fromJson(e)));
-    return plan;
+    if(json != null) {
+      Dialplan plan = new Dialplan();
+      if(json.containsKey('extensions')) {
+        plan.Extensions.addAll((json['extensions'] as List<Map>).map((Map e) => new Extension.fromJson(e)));
+      }
+
+      if(json.containsKey('number')) {
+        plan.entryNumber = json['number'];
+      }
+
+      if(json.containsKey('receptionid')) {
+        plan.receptionId = json['receptionid'];
+      }
+
+      return plan;
+    } else {
+      return null;
+    }
   }
 
-  Map toJson() {
-    return {'extensions': Extensions.map((Extension e) => e.toJson()).toList()};
-  }
+  Map toJson() => {
+    'extensions': Extensions.map((Extension e) => e.toJson()).toList()};
 }
