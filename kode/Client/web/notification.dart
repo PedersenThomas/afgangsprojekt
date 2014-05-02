18b2073bde756b871626eb19ed4fae5dcb7eb150
Thomas Pedersen <s103665@student.dtu.dk>
2014-05-02 20:23:02 +0200

    Notification box updated with click to remove feature.

--- kode/Client/web/notification.dart
+++ kode/Client/web/notification.dart
@@ -4,36 +4,65 @@ import 'dart:async';
 import 'dart:html';
 
 DivElement _box;
+cancellationToken _cancelToken;
 
 void initialize() {
   _box = new DivElement()
     ..classes.add('notificationbox');
 
   document.body.children.add(_box);
+
+  _box.onClick.listen((_) {
+    if(_cancelToken != null) {
+      _cancelToken.cancel();
+    }
+  });
+}
+
+void _deactivateBox(String cssClass) {
+  _box.classes.remove('notifyActivate');
+}
+
+void _activateBox(String cssClass) {
+  _box.classes
+    ..add('notifyActivate')
+    ..add(cssClass);
+
+  if(_cancelToken != null) {
+    _cancelToken.deactivateToken();
+  }
+
+  cancellationToken token = new cancellationToken(() => _deactivateBox(cssClass));
+  _cancelToken = token;
+
+  new Future.delayed(new Duration(milliseconds: 5000), () {
+    token.cancel();
+  });
 }
 
 void info(String text) {
   _box.text = text;
-  _box.classes
-    ..add('notifyActivate')
-    ..add('notificationboxinfo');
-
-  new Future.delayed(new Duration(milliseconds: 5000), () {
-    _box.classes
-      ..remove('notifyActivate')
-      ..remove('notificationboxinfo');
-  });
+  _activateBox('notificationboxinfo');
 }
 
 void error (String text) {
   _box.text = text;
-  _box.classes
-    ..add('notifyActivate')
-    ..add('notificationboxerror');
+  _activateBox('notificationboxerror');
+}
 
-  new Future.delayed(new Duration(milliseconds: 5000), () {
-    _box.classes
-      ..remove('notifyActivate')
-      ..remove('notificationboxerror');
-  });
+class cancellationToken {
+  bool cancelled = false;
+  Function onCancel;
+
+  cancellationToken(Function this.onCancel);
+
+  void cancel() {
+    if(!cancelled && onCancel != null) {
+      onCancel();
+    }
+  }
+
+  void deactivateToken() {
+    cancelled = true;
+  }
 }
