43db589df02bea210c246bd10fe03d6acbb218bf
Thomas Pedersen <s103665@student.dtu.dk>
2014-05-24 22:07:51 +0200

    Know your boundaries...

--- kode/Test/src/dialplan_tests.py
+++ kode/Test/src/dialplan_tests.py
@@ -64,8 +64,8 @@ class DialplanTests(unittest.TestCase):
         reception_id = 1
         response = self.adminServer.getDialplan(reception_id)[1]
         jsonBody = json.loads(response)
-        self.failIf('extensions' in jsonBody and len(jsonBody['extensions']) > 0)
-        if 'extensions' in jsonBody and len(jsonBody['extensions']) > 0:
+        self.failIf('extensions' in jsonBody and len(jsonBody['extensions']) == 0)
+        if 'extensions' in jsonBody and len(jsonBody['extensions']) == 0:
             self.fail('There is not a test dialplan to update.')
 
         oldValue = jsonBody['extensions'][0]['name']
